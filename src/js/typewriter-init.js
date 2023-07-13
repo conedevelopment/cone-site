4beaddbb5c37a236064771e2749dc3aaeb3eca89
Adam Laki <info@adamlaki.com>
2023-07-13 19:25:16 +0200

    Cleanup

---
+++ src/js/typewriter-init.js
@@ -0,0 +1,9 @@
+(() => {
+  new Typewriter('#typewriter', {
+    strings: ['as it should be', 'like a maniac', 'that helps you', 'like nobady\'s watching'],
+    autoStart: true,
+    cursor: '',
+    loop: true,
+    pauseFor: 2500,
+  });
+})();
