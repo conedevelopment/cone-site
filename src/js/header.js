5ecb7c5611685c557f2718659eccbbc2f1e4dbd8
Adam Laki <info@adamlaki.com>
2023-08-04 17:25:22 +0200

    Remove something from the header.js, but what?

--- src/js/header.js
+++ src/js/header.js
@@ -2,14 +2,11 @@
   let scrollpos = window.scrollY;
 
   const header = document.querySelector('.site-header');
-  const navigation = document.querySelector('.site-navigation');
   const scrollChange = 1;
 
   window.addEventListener('scroll', () => {
     scrollpos = window.scrollY;
-    setTimeout(() => {
-      navigation.style.setProperty('--inset-block-start', getComputedStyle(header).getPropertyValue('--block-size'));
-    }, 100);
+
     if (scrollpos >= scrollChange) {
       header.classList.add('site-header--sticky');
     } else {
