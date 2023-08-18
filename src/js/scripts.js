66b616bb81ee51ae72559a6be125b26a13e125e9
Adam Laki <info@adamlaki.com>
2023-08-18 20:13:53 +0200

    Add analytics tracking code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,5 +6,15 @@ if (
   issetCookie('cone-cookie-law-analytics')
   && getCookie('cone-cookie-law-analytics') === 'accepted'
 ) {
-  console.log('ANALYTICS');
+  const id = 'G-VG8YZ0GRD3';
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
+  document.head.append(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); } // eslint-disable-line
+  gtag('js', new Date());
+
+  gtag('config', 'G-VG8YZ0GRD3');
 }
